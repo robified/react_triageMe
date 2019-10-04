cd52620c703b8957f86e335852280ad3f6e97ea9
Robin <daorob@gmail.com>
2019-10-04 17:35:55 -0500

    Connect with MongoDB Atlas with success.

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true, 
     useCreateIndex: true, 
-    useUnifiedTopology: true});
+    useUnifiedTopology: true
+});
 
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
