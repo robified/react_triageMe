a03a2e329365566505c30a2a0484d1fa54ff6c9f
Robin <daorob@gmail.com>
2019-10-05 13:14:13 -0500

    Convert App component from fn to class component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Triage Me+!</h1>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Triage Me+!</h1>
+            </div>
+        )
+    }
 }
 
 export default App;
