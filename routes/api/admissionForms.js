6707e2ffb02d961d981976d72cdc0fab13290b21
Robin <daorob@gmail.com>
2019-10-04 21:53:59 -0500

    Add delete|put routes for admissionFormsCtrl.

--- routes/api/admissionForms.js
+++ routes/api/admissionForms.js
@@ -6,5 +6,7 @@ const admissionFormsCtrl = require('../../controllers/api/admissionForms');
 router.get('/', admissionFormsCtrl.index);
 router.get('/:id', admissionFormsCtrl.show);
 router.post('/', admissionFormsCtrl.create);
+router.delete('/:id', admissionFormsCtrl.delete);
+router.put('/:id', admissionFormsCtrl.update);
 
 module.exports = router;
