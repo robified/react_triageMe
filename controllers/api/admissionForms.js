c0a90c033492c4aa454576b06607dc2791bd42d1
Robin <daorob@gmail.com>
2019-10-04 21:34:37 -0500

    Create index|show fn for admissionForms.

--- controllers/api/admissionForms.js
+++ controllers/api/admissionForms.js
@@ -1,12 +1,22 @@
 const AdmissionForm = require('../../models/admissionForm');
 
 module.exports = {
+    index,
+    show,
     create
 };
 
+async function index(req, res) {
+    const admissionForms = await AdmissionForm.find({});
+    res.status(200).json(admissionForms);
+};
 
+async function show(req, res) {
+    const admissionForm = await AdmissionForm.findById(req.params.id);
+    res.status(200).json(admissionForm);
+};
 
 async function create(req, res) {
     const admissionForm = await AdmissionForm.create(req.body);
     res.status(201).json(admissionForm);
-}
+};
