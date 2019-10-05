7a99b5aaf9f346807adef64008af012f53b24e50
Robin <daorob@gmail.com>
2019-10-04 21:29:13 -0500

    Add index and show route for admissionFormsCtrl.

--- routes/api/admissionForms.js
+++ routes/api/admissionForms.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const admissionFormsCtrl = require('../../controllers/api/admissionForms');
 
 // GET /api/admissionForms
+router.get('/', admissionFormsCtrl.index);
+router.get('/:id', admissionFormsCtrl.show);
 router.post('/', admissionFormsCtrl.create);
 
 module.exports = router;
