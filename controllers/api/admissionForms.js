52cd8009a4ff22bdc94f9ec587f64c7f600e9321
Robin <daorob@gmail.com>
2019-10-04 21:52:36 -0500

    Create delete|update fn for admissionForms controller.

--- controllers/api/admissionForms.js
+++ controllers/api/admissionForms.js
@@ -3,7 +3,9 @@ const AdmissionForm = require('../../models/admissionForm');
 module.exports = {
     index,
     show,
-    create
+    create,
+    delete: deleteOne,
+    update
 };
 
 async function index(req, res) {
@@ -19,4 +21,14 @@ async function show(req, res) {
 async function create(req, res) {
     const admissionForm = await AdmissionForm.create(req.body);
     res.status(201).json(admissionForm);
+};
+
+async function deleteOne(req, res) {
+    const deletedAdmissionForm = await AdmissionForm.findByIdAndRemove(req.params.id);
+    res.status(200).json(deletedAdmissionForm);
+};
+
+async function update(req, res) {
+    const updatedAdmissionForm = await AdmissionForm.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(updatedAdmissionForm);
 };
