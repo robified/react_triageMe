463dafd3aacd37c867d8d106da51484917d85b92
Robin <daorob@gmail.com>
2019-10-06 16:24:50 -0500

    Create NavBar component. Import Route and NavBar component with path.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react'
-import logo from './logo.svg';
-
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
-
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
+    
+    /*---------- Lifecycle Methods ----------*/
+    componentDidMount() {
+        console.log('App: componentDidMount');
+    }
+     
     render() {
+        console.log('App: render');
         return (
             <div className="App">
-                <h1>Triage Me+!</h1>
+                <Route path='/navbar' render={(props) => (
+                    <NavBar {...props}/>
+                )}/>
+                
+                <header>
+                    <h1>App: Triage Me+!</h1>
+                </header>
             </div>
         )
     }
-}
+};
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = (props) => (
+    <div>
+        NavBar: I am a NavBar!
+    </div>
+)
+
+export default NavBar;
