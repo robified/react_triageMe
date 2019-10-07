eb70c48ba28e495d49b8cd24ef4b1e42cf6b1eeb
Robin <daorob@gmail.com>
2019-10-07 04:23:51 -0500

    Provide a JWT when a user signs up.

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+// Add the SECRET
+const SECRET = process.env.SECRET;
+
+module.exports = {
+  signup
+};
+
+async function signup(req, res) {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        const token = createJWT(user);
+        res.json({ token });
+    } catch (err) {
+        // Probably a duplicate email
+        res.status(400).json(err);
+    }
+}
+
+/*----- Helper Functions -----*/
+function createJWT(user) {
+    return jwt.sign(
+        {user}, // data payload
+        SECRET,
+        {expiresIn: '24h'}
+    );
+}
