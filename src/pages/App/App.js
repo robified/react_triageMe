da266a0869c81f961a730a1eaa6ab1af9f5a6b57
Robin <daorob@gmail.com>
2019-10-06 19:40:29 -0500

    Import Switch|AdmissionFormPage. Implement Switch|Route|AdmissionFormPage component.

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
+import AdmissionFormPage from '../AdmissionFormPage/AdmissionFormPage';
 
 class App extends Component {
     
@@ -12,15 +13,18 @@ class App extends Component {
      
     render() {
         console.log('App: render');
-        return (
+        return (     
             <div className="App component">
-                <Route path='/navbar' render={(props) => (
-                    <NavBar {...props}/>
-                )}/>
-                
-                <header>
-                    <h1>App: Triage Me+!</h1>
-                </header>
+                <header>App: Triage Me+!</header>
+                <Switch>
+                    <Route exact path='/' render={() =>
+                        <NavBar />    
+                    } />
+
+                    <Route exact path='/AdmissionFormPage' render={(props) => (
+                        <AdmissionFormPage {...props} />
+                    )}/>
+                </Switch>
             </div>
         )
     }
