a6b5a5d1f1d46b5fae5ab49d6c89109c10f2efa6
Robin <daorob@gmail.com>
2019-10-06 19:39:31 -0500

    Create AdmissionFormPage component.

---
+++ src/pages/AdmissionFormPage/AdmissionFormPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdmissionFormPage = props => {
+    return (
+        <div className="component">
+            <p>AdmissionFormPage: I am an AdmissionFormPage!</p>
+        </div>
+    );
+};
+
+export default AdmissionFormPage;
