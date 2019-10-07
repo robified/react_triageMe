0f9dd6556fd1d14362c2134005d9e852f42727c9
Robin <daorob@gmail.com>
2019-10-07 01:56:30 -0500

    Create an AboutPage component.

---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = props => {
+    return (
+        <div className="component">
+           <p>About Page: What about me?</p> 
+        </div>
+    );
+};
+
+export default AboutPage;
