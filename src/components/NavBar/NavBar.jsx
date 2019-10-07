5367503709c25f4ca3571512880fcb2825a28c23
Robin <daorob@gmail.com>
2019-10-07 01:53:52 -0500

    Create NavBar component. Import Link|NavBar.module.css. Create Link components for Home|About|Addmission Form|Sign Up|Login|Logout.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
 
 const NavBar = props => {
     return (
-        <div className="component">
-            <p>NavBar: I am a NavBar!</p>
+        <div className={`component ${styles.NavBar}`}>
+            <header>NavBar: I am a NavBar!</header>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/admissionform'>Admission Form</Link>
+            <Link to='/signup'>Sign Up</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/logout'>Logout</Link>
         </div>
     );
 };
