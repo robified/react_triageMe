8ccfc128dfaf23238cb3f1670b2ed0cc703a9bba
Robin <daorob@gmail.com>
2019-10-08 14:12:04 -0500

    Add NavBar component to AdmissionFormPage.

--- src/pages/AdmissionFormPage/AdmissionFormPage.jsx
+++ src/pages/AdmissionFormPage/AdmissionFormPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar';
 
 const AdmissionFormPage = props => {
     return (
         <div className="component">
+            <NavBar />
             <p>AdmissionFormPage: I am an AdmissionFormPage!</p>
         </div>
     );
