2e2f7adc033a3b0ce03edcdfc0cf0097469ba2bf
Robin <daorob@gmail.com>
2019-10-18 19:56:12 -0500

    Remove NavBar component.

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import NavBar from '../../components/NavBar/NavBar';
 
 const AboutPage = props => {
     return (
         <div className="component">
-            <NavBar/>
             <p>About Page: What about me?</p> 
         </div>
     );
